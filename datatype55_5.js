84f504329e6e1647ee351f89702bb533e7f6bf95
koji noguchi <info@interight.co.jp>
2023-02-28 10:29:29 +0000

    add 55_5 file

---
+++ datatype55_5.js
@@ -0,0 +1,11 @@
+["Bilbo", "Gandalf", "Nazgul"].forEach((item, index, array) => {
+  console.log(`${item} is at index ${index} in ${array}`)
+});
+
+(item) => {
+ console.log(item)
+}
+
+function arry2(item) {
+  console.log(item)
+}
