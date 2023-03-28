2bb46ae9759e89719f2b527a0652392b803bef58
koji noguchi <info@interight.co.jp>
2023-03-28 10:27:00 +0000

    add datatype512_1

---
+++ datatype512_1.js
@@ -0,0 +1,14 @@
+let user = {
+  name: "John",
+  age: 30
+};
+
+let json = JSON.stringify(user);
+
+
+let str = `{"name": "John", "age":30}`;
+
+console.log(str);
+console.log(json);
+console.log(typeof json);
+
