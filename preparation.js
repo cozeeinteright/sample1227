82b05862e790be6ee3384c0ab70a22147830e374
koji noguchi <info@interight.co.jp>
2023-04-25 10:03:33 +0000

    add jsinfo6_8_1

--- preparation.js
+++ preparation.js
@@ -1,5 +1,30 @@
-function sayHi() {
-  console.log('Hello');
+let worker = {
+  someMethod() {
+    return 1;
+  },
+
+  slow(x) {
+    // scary CPU-heavy task here
+    console.log("Called with " + x);
+    return x * this.someMethod(); // (*)
+  }
+};
+
+// same code as before
+function cachingDecorator(func) {
+  let cache = new Map();
+  return function(x) {
+    if (cache.has(x)) {
+      return cache.get(x);
+    }
+    let result = func.call(this,x); // (**)
+    cache.set(x, result);
+    return result;
+  };
 }
 
-setTimeout(sayHi, 1000);
+console.log( worker.slow(1) ); // the original method works
+
+worker.slow = cachingDecorator(worker.slow); // now make it caching
+
+console.log( worker.slow(2) );
