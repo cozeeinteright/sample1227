5b47ee43f39eb4e0e8b9f752e337bec04df4094a
koji noguchi <info@interight.co.jp>
2023-06-13 10:54:05 +0000

    jsinfo7_2_2.js

---
+++ jsinfo7_2_2.js
@@ -0,0 +1,20 @@
+let user = {
+  get name() {
+    return this._name;
+  },
+
+  set name(value) {
+    if(value.length < 4) {
+      console.log("Name in too short, need at leat 4");
+      return;
+    }
+    this._name = value;
+  }
+};
+
+
+user.name = "Pete";
+console.log(user.name);
+
+user.name = ""
+console.log(user.name);
