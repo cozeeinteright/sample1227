1f9323f7b5a159c96cb4d876916f9514fc0c8596
koji noguchi <info@interight.co.jp>
2023-06-27 10:08:00 +0000

    add jsinfo10_1_2

---
+++ jsinfo10_1_2.js
@@ -0,0 +1,14 @@
+let json = '{ "age": 30}';
+
+try {
+
+  let user = JSON.parse(json);
+
+  if(!user.name) {
+    throw new SyntaxError("Incomplete data: no name");
+  }
+
+  console.log(user.name);
+} catch(e) {
+  console.log("JSON Error: " + e.message );
+} 
